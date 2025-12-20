185cdbad3fddf5eebd8606148c5a0b5d18375cf4
antonio <anponioparques@gmail.com>
2025-12-20 17:10:44 -0400

    Correcting visualization (color, layout, and image)

--- app/src/main/java/com/example/basicmath/activities/ModesActivity.java
+++ app/src/main/java/com/example/basicmath/activities/ModesActivity.java
@@ -6,6 +6,9 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,6 +26,7 @@ public class ModesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_modes);
@@ -59,6 +63,16 @@ public class ModesActivity extends AppCompatActivity {
         System.out.println("AQUI 1 0");
         recyclerView.setAdapter(adapter);
         System.out.println("setou adapter");
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+            );
+            return insets;
+        });
     }
 
     private ModeInfo criarMode(Class<?> target, int image){
